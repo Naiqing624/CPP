5aaa5e534aad364b1a49ba68ec9789cd2b8c51fb
Naiqing Cao <nacao@c1r5s3.42luxembourg.lu>
2025-03-06 17:08:51 +0100

    cpp00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nacao <nacao@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 14:01:31 by nacao             #+#    #+#             */
+/*   Updated: 2025/03/06 14:28:05 by nacao            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc <= 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j] != '\0'; j++)
+			{
+				std::cout << (char)std::toupper(argv[i][j]);
+			}
+			if (i < argc - 1)
+					std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
